8e41edcb7ab2f663a9c4d8d9c1147a45a297fca5
Denis Potapov <denis.potapov@esrlabs.com>
2020-03-28 23:51:50 +0100

    A little memo about usage

--- README.md
+++ README.md
@@ -1,5 +1,86 @@
 # Websocket sandbox
 
-- http://localhost:5123/
-- http://localhost:5124/ - through nginx
-- http://localhost:5125/ - through nginx-ingress
+## Vagrant
+
+To create VM
+
+```
+vagrant up
+```
+
+To connect to VM
+
+```
+vagrant ssh
+```
+
+To destroy
+
+```
+vagrant destroy
+```
+
+
+## Vagrant VM provisioning
+
+`provision.sh`
+
+It installs:
+
+- node.js
+- nginx
+- docker
+- toy docker registry (`docker-registry.service`)
+- kubernetes + nginx ingress controller (exposed with host network and deployed as daemonset `ingress-host-network.yaml`)
+
+## Toy app
+
+Very simple toy application that uses websockets.
+
+```
+cd app/src
+npm install
+node server.js
+```
+
+Exposed on 8080 port inside Vagrant VM
+Forwarded to local port http://localhost:5123/
+
+To build docker image and push it registry (required for kubernetes deployment):
+
+```
+cd app
+./build.sh
+```
+
+
+## Proxy with nginx
+
+Idea is to check minimal required nginx setup to be able to proxy websockets.
+
+Nginx may fail to start automatically, because of `/vagrant` is mounted later sometimes. Just `service nginx start`.
+
+`nginx.conf` linked to nginx config dir
+
+Listening on 81 port.
+Forwarded to http://localhost:5124/ on host
+
+
+## Deploy app to kubernetes and expose with ingress
+
+Idea is to check if websockets are forwarded by default by ingress controller or identify what minimally needs to be added.
+
+Manifest file `kube-ws-app.yaml`. It creates:
+
+- deployment
+- service
+- ingress
+
+Deploy:
+
+```
+kubectl apply -f kube-ws-app.yaml
+```
+
+Exposed on 80 port of VM
+Forwarded to http://localhost:5125/
--- provision.sh
+++ provision.sh
@@ -1,55 +1,81 @@
 #!/bin/bash
 
-NODE_VERSION="v12.16.1"
 
 
-apt update
+main () {
+	apt update
+	mkdir -p /tmp/distr
+	cd /tmp/distr
+	install
+}
 
-mkdir -p /tmp/distr
-cd /tmp/distr
+install() {
+	install_node
+	install_nginx
+	install_docker
+	setup_registry_service
+	install_kubernetes
+	setup_ingress
+}
 
-fname="node-${NODE_VERSION}-linux-x64.tar.xz"
-test -e "${fname}" \
-|| wget -q "https://nodejs.org/dist/${NODE_VERSION}/${fname}"
-mkdir -p /opt/nodejs
-tar -xJf "${fname}" -C /opt/nodejs
+install_node() {
+	local NODE_VERSION="v12.16.1"
+	fname="node-${NODE_VERSION}-linux-x64.tar.xz"
+	test -e "${fname}" \
+	|| wget -q "https://nodejs.org/dist/${NODE_VERSION}/${fname}"
+	mkdir -p /opt/nodejs
+	tar -xJf "${fname}" -C /opt/nodejs
+	profile_str="export PATH=/opt/nodejs/node-${NODE_VERSION}-linux-x64/bin:\$PATH"
+	grep "$profile_str" "$HOME/.bashrc" >/dev/null \
+	|| echo "$profile_str" >> "$HOME/.bashrc"
+	eval "$profile_str"
+}
 
-profile_str="export PATH=/opt/nodejs/node-${NODE_VERSION}-linux-x64/bin:\$PATH"
-grep "$profile_str" "$HOME/.bashrc" >/dev/null \
-|| echo "$profile_str" >> "$HOME/.bashrc"
-eval "$profile_str"
+install_nginx() {
+	apt install -y nginx
+	unlink /etc/nginx/sites-enabled/default
+	ln -s /vagrant/nginx.conf /etc/nginx/sites-available/websocket-standbox
+	ln -s /etc/nginx/sites-available/websocket-standbox /etc/nginx/sites-enabled/websocket-standbox
+	service nginx restart
+}
 
-apt install -y nginx
-unlink /etc/nginx/sites-enabled/default
-ln -s /vagrant/nginx.conf /etc/nginx/sites-available/websocket-standbox
-ln -s /etc/nginx/sites-available/websocket-standbox /etc/nginx/sites-enabled/websocket-standbox
-service nginx restart
+install_docker() {
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sh get-docker.sh
+}
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+setup_registry_service() {
+	ln -s "/vagrant/docker-registry.service" /etc/systemd/system/
+	systemctl enable docker-registry.service
+	systemctl start docker-registry.service
+}
 
-ln -s "$(pwd)/docker-registry.service" /etc/systemd/system/
-systemctl enable docker-registry.service
-systemctl start docker-registry.service
+install_kubernetes() {
+	cat > /etc/sysctl.d/k8s.conf <<-EOF
+		net.bridge.bridge-nf-call-ip6tables = 1
+		net.bridge.bridge-nf-call-iptables = 1
+	EOF
+	sysctl --system
 
-cat > /etc/sysctl.d/k8s.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-sysctl --system
+	curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+	cat >/etc/apt/sources.list.d/kubernetes.list <<-EOF 
+		deb https://apt.kubernetes.io/ kubernetes-xenial main
+	EOF
+	apt update
+	apt install -y kubelet kubeadm kubectl
+	apt-mark hold kubelet kubeadm kubectl
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt update
-apt install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+	kubeadm init
+	mkdir -p $HOME/.kube
+	cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+	chown $(id -u):$(id -g) $HOME/.kube/config
+	kubectl taint node ubuntu-bionic node-role.kubernetes.io/master:NoSchedule-
+	kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+}
 
-kubeadm init
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl taint node ubuntu-bionic node-role.kubernetes.io/master:NoSchedule-
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+setup_ingress() {
+	kubectl apply -f "/vagrant/ingress-host-network.yaml"
+}
 
+
+main
